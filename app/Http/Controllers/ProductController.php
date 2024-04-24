9eb59d5492e1ade6423621f1959153e3bf4680bd
xllyyyyy <johnellyaltarejos69@gmail.com>
2024-04-24 15:08:51 +0800

    act7_altarejos

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = "Product list form in ProductController";
+        return view('products.index', ['products' =>$products]);
+    }
+
+    public function create()
+    {
+        return view ('products.create');
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductController::class);
